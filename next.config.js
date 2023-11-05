90a57ac59e59a0a249531b26cad968395cf23b1a
whoawora <worawib.official@gmail.com>
2023-11-06 01:52:11 +0700

    edit gh

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    assetPrefix: './',
+    images: {
+      unoptimized: true
+    }
+}
 
 module.exports = nextConfig
