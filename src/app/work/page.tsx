60fad9c2937cf81a6c2e2dd26de094cf6855b30e
whoawora <worawib.official@gmail.com>
2023-11-15 00:42:23 +0700

    fix: edit work page

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -9,23 +9,25 @@ type Props = {}
 
 export default function page({}: Props) {
   return (
-    <div>
-    <div className='container mx-auto'>
-    <div className="grid grid-cols-1 gap-4 pt-16">
-      
-      <div className="work-content" data-aos="fade-up">
-        <Link href="work/tawiset">
-        <Image
-        src={tawisetimg}
-        alt="Picture of the author"
-        className='rounded-3xl'
-      />
-        </Link>
-    
-      </div> 
-      
-    </div>
-    </div>
+    <div className='bg-black'>
+      <div className='px-6 xl:px-28  mx-auto'>
+        <div className="grid grid-cols-1 gap-4 pt-16 bg-black">
+          <div className="work-content" data-aos="fade-up">
+              <Link href="work/tawiset">
+              <Image
+              src={tawisetimg}
+              alt="Picture of the author"
+              className='rounded-3xl'
+            />
+              </Link>       
+          </div> 
+          <div className="work-content" data-aos="fade-up">
+        
+          </div>
+          <br />
+          <br />
+        </div>
+      </div>
     </div>
   )
 }
