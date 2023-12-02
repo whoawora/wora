026b36a7f9c410a534ef6e36792db2bdd13b7851
whoawora <worawib.official@gmail.com>
2023-12-02 20:22:05 +0700

    fix: edit react strict mode

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   output: "export",
-    images: {
-      unoptimized: true
+  reactStrictMode: true,
+  output: "export",
+  images: {
+    unoptimized: true
   }
 };
 
