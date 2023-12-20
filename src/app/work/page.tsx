f443b24723f6c3ef4702246d05ed0f4f99529eba
whoawora <worawib.official@gmail.com>
2023-12-21 01:04:26 +0700

    fix: edit work

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -6,6 +6,8 @@ import tawisetimg from '@/image/tawiset-cover.png'
 import TawisetDetail from '@/image/tawiset-more.jpeg'
 import Furcover from '@/image/furniture-cover.png'
 import Furdetail from '@/image/fur-detail.jpg'
+import Tcdccover from '@/image/tcdc-cover.jpg'
+import TcdcDetail from '@/image/tcdc.jpeg'
 import { motion } from 'framer-motion';
 import { imgSlide } from './anim';
 import Modal from '@mui/joy/Modal';
@@ -18,6 +20,7 @@ type Props = {}
 export default function Work({}: Props) {
   const [open, setOpen] = React.useState<boolean>(false);
   const [open1, setOpen1] = React.useState<boolean>(false);
+  const [open2, setOpen2] = React.useState<boolean>(false);
 
   const [numPages, setNumPages] = useState(null)
   const onDocumentSuccess = ({numPages}: {numPages:any}) => {
@@ -38,7 +41,7 @@ export default function Work({}: Props) {
               </motion.div> 
             </div>
             <Modal open={open} onClose={() => setOpen(false)} className="modal-work" sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center',overflow:"hidden"}}>
-              <Sheet className="scroll-hide"  sx={{ maxWidth: "70%",  maxHeight: "90vh",  backgroundColor:"#17132D", outline:'none', border:"1px solid white", borderRadius: '10px', overflowY:"scroll"}}>
+              <Sheet className="scroll-hide"  sx={{ maxWidth: "80%",  maxHeight: "90vh",  backgroundColor:"#17132D", outline:'none', border:"1px solid white", borderRadius: '10px', overflowY:"scroll"}}>
                 <ModalClose variant="plain" sx={{ m: 1 , backgroundColor: "#fff", position: "sticky"}} />
                 <Image src={TawisetDetail} alt="Picture of the author"  className='rounded-3xl' />
               </Sheet>
@@ -55,11 +58,33 @@ export default function Work({}: Props) {
           </motion.div> 
         </div>
         <Modal open={open1} onClose={() => setOpen1(false)} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center',overflow:"hidden" }}>
-        <Sheet className="scroll-hide"  sx={{ maxWidth: "70%",  maxHeight: "90vh",  backgroundColor:"#fff", outline:'none', border:"1px solid white", borderRadius: '10px', overflowY:"scroll"}}>
+        <Sheet className="scroll-hide"  sx={{ maxWidth: "80%",  maxHeight: "90vh",  backgroundColor:"#fff", outline:'none', border:"1px solid white", borderRadius: '10px', overflowY:"scroll"}}>
           <ModalClose variant="plain" sx={{ m: 1 , backgroundColor: "#fff", position: "sticky"}} />
-          <Image src={Furdetail} alt="Picture of the author"  className='rounded-3xl' />
+          <Image src={Furdetail} alt="Picture of the author" />
           </Sheet>
         </Modal>
+
+
+        <div color="neutral" onClick={() => setOpen2(true)}>
+        <motion.div variants={imgSlide} initial="initial" animate="enter" exit="exit">
+          <div className="work-content rounded-3xl bg-neutral-900 w-full h-full" data-aos="fade-up">
+              <Image src={Tcdccover} alt="Picture of the author" className='rounded-3xl' />
+          </div> 
+          </motion.div> 
+        </div>
+        <Modal open={open2} onClose={() => setOpen2(false)} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center',overflow:"hidden" }}>
+        <Sheet className="scroll-hide"  sx={{ maxWidth: "80%",  maxHeight: "90vh",  backgroundColor:"#fff", outline:'none', border:"1px solid white", borderRadius: '10px', overflowY:"scroll"}}>
+          <ModalClose variant="plain" sx={{ m: 1 , backgroundColor: "#fff", position: "sticky"}} />
+          <Image src={TcdcDetail} alt="Picture of the author" />
+          </Sheet>
+        </Modal>
+
+
+
+
+
+
+
         </div> 
       </div>
         <br />
